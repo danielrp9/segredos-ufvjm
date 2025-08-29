3846230808157f841aaa81c5d5b9be7d26114ca8
Daniel Rodrigues <danielrodrigues878@hotmail.com>
2025-08-29 13:55:12 -0300

    correção de administrador

--- fofocas/admin.py
+++ fofocas/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# fofocas/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Fofoca, Comentario
+
+# Registra o modelo Fofoca
+@admin.register(Fofoca)
+class FofocaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_publicacao', 'tags')
+    list_filter = ('tags', 'data_publicacao')
+    search_fields = ('titulo', 'corpo')
+
+# Registra o modelo Comentario
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('fofoca', 'data_comentario', 'parent_comment')
+    list_filter = ('fofoca', 'data_comentario')
+    search_fields = ('corpo',)
